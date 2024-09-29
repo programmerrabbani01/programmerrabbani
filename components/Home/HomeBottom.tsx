3ec7153dd66c5cb2495dd4149085f47715ba6367
programmerrabbani01 <programmerrabbani@gmail.com>
2024-09-29 12:14:20 +0600

    Home Page Fixing Done

--- components/Home/HomeBottom.tsx
+++ components/Home/HomeBottom.tsx
@@ -5,6 +5,7 @@ import Coverflow from "../Coverflow/Coverflow.tsx";
 import Link from "next/link";
 import BlogCard from "./BlogCard.tsx";
 import { blogItems } from "@/data/Blog.tsx";
+import WorkingTogether from "./WorkingTogether.tsx";
 
 type Props = {};
 
@@ -89,32 +90,7 @@ export default function HomeBottom({}: Props) {
       <div className="mt-6">
         <Card>
           <div className="p-6">
-            <div className="bg-[#f0f2f5] dark:bg-[#0e1018] rounded-lg overflow-hidden p-6">
-              <div className="flex items-center gap-6">
-                {/* slider item */}
-                <div className="inline-block auto_scroll_list text_slider whitespace-nowrap">
-                  {/* Duplicate links for sliding effect */}
-                  {Array(2)
-                    .fill(null)
-                    .map((_, index) => (
-                      <div key={index} className="inline-block">
-                        <Link
-                          href=""
-                          className="inline-block text-[30px] md:text-[40px] lg:text-[40px] xl:text-[40px] font-medium text-[#576076] leading-[.9em] mx-[15px] relative before:absolute before:w-[10px] before:h-[10px] before:bg-[#576076] before:rounded-full before:top-1/2 before:left-[-32px] before:transform before:-translate-y-1/2 hover:text-[#1a1f2c] dark:hover:text-white transition-all duration-300 mr-10"
-                        >
-                          Let's 👋 Work Together
-                        </Link>
-                        <Link
-                          href=""
-                          className="inline-block text-[30px] md:text-[40px] lg:text-[40px] xl:text-[40px]  font-medium text-[#576076] leading-[.9em] mx-[15px] relative before:absolute before:w-[10px] before:h-[10px] before:bg-[#576076] before:rounded-full before:top-1/2 before:left-[-32px] before:transform before:-translate-y-1/2 hover:text-[#1a1f2c] dark:hover:text-white transition-all duration-300 mr-10 "
-                        >
-                          Let's 👋 Work Together
-                        </Link>
-                      </div>
-                    ))}
-                </div>
-              </div>
-            </div>
+            <WorkingTogether />
           </div>
         </Card>
       </div>
