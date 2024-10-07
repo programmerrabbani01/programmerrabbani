dc99e85380154d4e67ace1bf22d992758c37518a
programmerrabbani01 <programmerrabbani@gmail.com>
2024-10-07 11:39:19 +0600

    Projects Add In Project Card Done

--- data/Portfolio.tsx
+++ data/Portfolio.tsx
@@ -1,4 +1,4 @@
-import project1 from "@/public/images/download.png";
+import project1 from "@/public/images/projects/project-1.jpeg";
 import project2 from "@/public/images/projects/project-2.png";
 import project3 from "@/public/images/projects/project-3.png";
 
@@ -21,23 +21,6 @@ export const projects = [
     name: "FlowArch - Architecture Service Website",
     category: "UI UX Design | Development",
     imgSrc: project3,
-  },
-  {
-    id: 4,
-    name: "FlowSaaS - SaaS Application Tools",
-    category: "SaaS App | UI UX Design",
-    imgSrc: project1,
-  },
-  {
-    id: 5,
-    name: "AIMug - AI Writing Application Tools",
-    category: "Product Design | Application",
-    imgSrc: project2,
-  },
-  {
-    id: 6,
-    name: "FlowArch - Architecture Service Website",
-    category: "UI UX Design | Development",
-    imgSrc: project3,
-  },
+  }
+  
 ];
