589eda09f89d63ca7bfc70b82c54c12b5b7c237b
programmerrabbani01 <programmerrabbani@gmail.com>
2024-10-07 20:26:29 +0600

    All Projects Add On HomePage Done

--- data/HomeProjects.tsx
+++ data/HomeProjects.tsx
@@ -3,6 +3,13 @@ import webdev from "@/public/images/projects/project-2.jpeg";
 import chawala from "@/public/images/projects/project-3.jpeg";
 import healthcare from "@/public/images/projects/project-4.jpeg";
 import timekeeper from "@/public/images/projects/project-5.jpeg";
+import travelAgency from "@/public/images/projects/project-6.jpeg";
+import hondacbr from "@/public/images/projects/project-7.jpeg";
+import school from "@/public/images/projects/project-8.jpeg";
+import hotGadgets from "@/public/images/projects/project-9.jpeg";
+import panda from "@/public/images/projects/project-10.jpeg";
+import hermes from "@/public/images/projects/project-11.jpeg";
+import social from "@/public/images/projects/project-12.jpeg";
 import { StaticImageData } from "next/image";
 
 type Project = {
@@ -77,5 +84,89 @@ export const homeProjects: Project[] = [
     ],
     projectLink: "https://timekeeper-bfc3b.web.app/",
     githubLink: "https://github.com/programmerrabbani01/timekeeper",
+  },
+  {
+    id: 6,
+    src: travelAgency,
+    alt: "travelAgency",
+    title: "TravelAgency Website Development",
+    description:
+      "This project is built using React, React Router Dom, React Rating Stars, React Bootstrap and  Bootstrap, combining powerful technologies to create a modern, responsive, and secure web application.",
+    technologies: [
+      "React",
+      "Bootstrap",
+      "React Router Dom",
+      "React Bootstrap",
+      "React Rating Stars",
+    ],
+    projectLink: "https://travelagency-31f17.web.app/",
+    githubLink: "https://github.com/programmerrabbani01/TravelAgency",
+  },
+  {
+    id: 7,
+    src: hondacbr,
+    alt: "hondacbr",
+    title: "Honda CBR Website Development",
+    description: "Figma To HTML",
+    technologies: ["Figma", "Jquery", "Bootstrap", "HTML", "CSS3"],
+    projectLink: "https://programmerrabbani01.github.io/honda-cbr/",
+    githubLink: "https://github.com/programmerrabbani01/honda-cbr",
+  },
+  {
+    id: 8,
+    src: school,
+    alt: "school",
+    title: "School Resources Website Development",
+    description: "Figma To HTML",
+    technologies: ["Figma", "Jquery", "Bootstrap", "HTML", "CSS3"],
+    projectLink: "https://programmerrabbani01.github.io/school-resources/",
+    githubLink: "https://github.com/programmerrabbani01/school-resources",
+  },
+  {
+    id: 9,
+    src: hotGadgets,
+    alt: "hotGadgets",
+    title: "Hot Gadgets Website Development",
+    description: "Figma To HTML",
+    technologies: ["Figma", "Jquery", "Bootstrap", "HTML", "CSS3"],
+    projectLink: "https://programmerrabbani01.github.io/hot-gadgets/",
+    githubLink: "https://github.com/programmerrabbani01/hot-gadgets",
+  },
+  {
+    id: 10,
+    src: panda,
+    alt: "panda",
+    title: "Panda Commerce Website Development",
+    description: "Figma To HTML",
+    technologies: ["Figma", "Jquery", "Bootstrap", "HTML", "CSS3"],
+    projectLink: "https://programmerrabbani01.github.io/panda-commerce/",
+    githubLink: "https://github.com/programmerrabbani01/panda-commerce",
+  },
+  {
+    id: 11,
+    src: hermes,
+    alt: "hermes",
+    title: "Hermes HTML Template Development",
+    description: "Figma To HTML",
+    technologies: [
+      "Figma",
+      "Javascript",
+      "Slick Slider",
+      "Bootstrap",
+      "HTML",
+      "CSS3",
+    ],
+    projectLink: "https://programmerrabbani01.github.io/hermes-HTML-template/",
+    githubLink: "https://github.com/programmerrabbani01/hermes-HTML-template",
+  },
+  {
+    id: 12,
+    src: social,
+    alt: "social",
+    title: "Social Website Development",
+    description: "Figma To HTML",
+    technologies: ["Figma", "Jquery", "HTML", "CSS3"],
+    projectLink: "https://programmerrabbani01.github.io/social/",
+    githubLink: "https://github.com/programmerrabbani01/social",
   },
 ];
