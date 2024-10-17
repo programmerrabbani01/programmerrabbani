7c185c675b18a077b1ac1d1698d98c91e8a842a5
programmerrabbani01 <programmerrabbani@gmail.com>
2024-10-17 18:47:29 +0600

    Upgrading Running

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
         myPrimary: "#4770ff",
         myPrimaryHover: "rgba(13,110,253,1)",
         myGray: "#576076",
-        myBgLight:"#f0f2f5",
-        myBgDark:"#0e1018",
-        myBgDarkTwo:"#1a1f2c",
-        myHoverPrimary:"#2563eb",
-        myBorder:"#dbdfe5",
-        myRgbaLight:"rgba(240,242,245,.7)",
-        myDarkText:"#64748b",
-        myRgbaDark:"rgba(87,96,118,.5)",
-        myTextDark:"#64748b",
-        myDarkTextTwo:"#616670",
-        myDarkTextThree:"#d1d5db",
-        myBgDarkThree:"#4b5563"
+        myBgLight: "#f0f2f5",
+        myBgDark: "#0e1018",
+        myBgDarkTwo: "#1a1f2c",
+        myHoverPrimary: "#2563eb",
+        myBorder: "#dbdfe5",
+        myRgbaLight: "rgba(240,242,245,.7)",
+        myDarkText: "#64748b",
+        myRgbaDark: "rgba(87,96,118,.5)",
+        myTextDark: "#64748b",
+        myDarkTextTwo: "#616670",
+        myDarkTextThree: "#d1d5db",
+        myBgDarkThree: "#4b5563",
       },
       fontFamily: {
         bricolageGrotesque: ["var(--font-bricolageGrotesque)", "sans-serif"],
